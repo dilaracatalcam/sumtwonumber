b9baaf2e0dc5797e3afad91432f92fdb435eb0da
dilaracatalcam <117274714+dilaracatalcam@users.noreply.github.com>
2022-11-02 23:19:53 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+
+    int num1,num2,sum;
+
+
+    printf("Please enter first number = ");
+    scanf("%d",&num1);
+    printf("Please enter second number = ");
+    scanf("%d",&num2);
+
+    sum = num1 + num2;
+    printf("%d + %d = %d ",num1,num2,sum);
+
+
+
+
+
+
+
+
+    return 0;
+}
